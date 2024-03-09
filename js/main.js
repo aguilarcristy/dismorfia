06dbbf6b5d61661babdb643c2df386c7ed74fabf
aguilarcristy <cristyaguilar3101@gmail.com>
2024-03-09 11:01:12 -0800

    Pushing foundation

---
+++ js/main.js
@@ -0,0 +1,7 @@
+console.log("dismorfia");
+
+// Click Event on Header Changes Background Color
+document.querySelector("media/Scalpel.png").onclick = function (){
+    // console.log("clicked header");
+    document.querySelector("body").style.backgroundColor = "black";
+}
